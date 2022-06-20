e9ff4cea9c9dd9edffbda7fd6104dd25b0f0bb67
Renan Araujo <ra.carvalho@unesp.br>
2022-06-20 11:33:51 -0300

    paginate, data verification and get username by header

--- index.js
+++ index.js
@@ -22,6 +22,39 @@ const users = [
 ];
 const tweets = [
     {username: "bobesponja", tweet: "eu amo o hub"},
+    {username: "narutin", tweet: "SASKEEEE1"},
+    {username: "saske", tweet: "NARUTOOOOO"},
+    {username: "narutin", tweet: "SASKEEEEEEEEEEE"},
+    {username: "saske", tweet: "NARUTOOOoOOo"},
+    {username: "narutin", tweet: "SASKEEEeeEEEEEeEEEeeE"},
+    {username: "saske", tweet: "NARUTOOOOOoooooooooOOoo"},
+    {username: "narutin", tweet: "SASKEEEEEEEEEEEEEEEEEE"},
+    {username: "saske", tweet: "NARUTOOOOOOOOOOOOOOOOO"},
+    {username: "narutin", tweet: "SASKE?"},
+    {username: "saske", tweet: "NARUTO."},
+    {username: "bobesponja", tweet: "eu amo o hub2"},
+    {username: "narutin", tweet: "SASKEEEE"},
+    {username: "saske", tweet: "NARUTOOOOO"},
+    {username: "narutin", tweet: "SASKEEEEEEEEEEE"},
+    {username: "saske", tweet: "NARUTOOOoOOo"},
+    {username: "narutin", tweet: "SASKEEEeeEEEEEeEEEeeE"},
+    {username: "saske", tweet: "NARUTOOOOOoooooooooOOoo"},
+    {username: "narutin", tweet: "SASKEEEEEEEEEEEEEEEEEE"},
+    {username: "saske", tweet: "NARUTOOOOOOOOOOOOOOOOO"},
+    {username: "narutin", tweet: "SASKE?"},
+    {username: "saske", tweet: "NARUTO."},
+    {username: "bobesponja", tweet: "eu3 amo o hub3"},
+    {username: "narutin", tweet: "SASKEEEE"},
+    {username: "saske", tweet: "NARUTOOOOO"},
+    {username: "narutin", tweet: "SASKEEEEEEEEEEE"},
+    {username: "saske", tweet: "NARUTOOOoOOo"},
+    {username: "narutin", tweet: "SASKEEEeeEEEEEeEEEeeE"},
+    {username: "saske", tweet: "NARUTOOOOOoooooooooOOoo"},
+    {username: "narutin", tweet: "SASKEEEEEEEEEEEEEEEEEE"},
+    {username: "saske", tweet: "NARUTOOOOOOOOOOOOOOOOO"},
+    {username: "narutin", tweet: "SASKE?"},
+    {username: "saske", tweet: "NARUTO."},
+    {username: "bobesponja", tweet: "eu4 amo o hub4"},
     {username: "narutin", tweet: "SASKEEEE"},
     {username: "saske", tweet: "NARUTOOOOO"},
     {username: "narutin", tweet: "SASKEEEEEEEEEEE"},
@@ -36,12 +69,31 @@ const tweets = [
 ];
 
 server.post("/sign-up", (req, res)=>{
+    
+    function isValidateURL(value) {
+        return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(value);
+    }
+
+    if(req.body.username.length < 1 || !isValidateURL(req.body.avatar)){
+        if(!isValidateURL(req.body.avatar) && req.body.avatar.length > 0){
+            res.status(400).send("Url Invalida!")
+        }
+        res.status(400).send("Todos os campos são obrigatórios!")
+    }
     users.push(req.body);
-    res.send("OK");
+    res.status(201).send("OK");
 })
 
 server.get("/tweets", (req,res)=>{
-    const lastTweets = tweets.slice(-10).reverse().map((tweet) => { //Usei o reverse para os tweets do usuário aparecer no top, igual no twitter. 
+    const page = req.query.page
+    if(page < 1 ){
+
+        res.status(400).send("Informe uma página válida!")
+    }
+    function paginate(array, page_size, page_number) {
+        return array.slice((page_number - 1) * page_size, page_number * page_size);
+      }
+    const lastTweets = paginate(tweets.slice().reverse(),10,page).map((tweet) => { 
         const userTweet = users.find((user)=> user.username === tweet.username);
         return ({
         "username": tweet.username,
@@ -52,8 +104,15 @@ server.get("/tweets", (req,res)=>{
     res.send(lastTweets);
 })
 server.post("/tweets", (req,res)=>{
-    tweets.push(req.body);
-    res.send("OK");
+    if(req.headers.user.length < 1 || req.body.tweet.length < 1){
+        res.status(400).send("Todos os campos são obrigatórios!")
+    }
+    const newTweet = {
+        username: req.headers.user,
+        tweet: req.body.tweet
+    }
+    tweets.push(newTweet);
+    res.status(201).send("OK");
 })
 
 server.listen(5000);
